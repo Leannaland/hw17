287127a38aa7abd0089929b2c6a66b4bc9c30a70
Leannaland <leannaland2011@gmail.com>
2020-12-23 18:04:48 -0500

    hw

---
+++ HomeworkWeek17/README.md
@@ -0,0 +1,60 @@
+
+
+ 
+ 
+  ### Created by 
+  
+  Leanna Land
+
+  ## Description
+ 
+  This is a fitness tracker that let's the user keep track of their exercise's and activities then display's workout in graphs. 
+  
+  ## Table of Contents
+
+  * [Installation] 
+
+  * [Usage] 
+  
+  * [License] 
+  
+  * [Contributing] 
+  
+  * [Tests] 
+
+  *[Questions]
+
+  ## Installation
+  *Steps required to install project and how to get the development environment running:* 
+  
+  npm install
+
+  ## Technologies
+  *Instructions and examples for use*
+
+  MongoDB
+  Mongoose
+  Node.js
+  
+  
+
+  ## License
+  *The License you used for the project*
+ 
+  
+
+  ## contributors
+  *If you are going to contribute it, you can follow these guidelines for how to do so*
+  
+  You can do anything 
+
+  ## Testing
+  *Tests for application and how to run them:*
+  
+  
+
+  ## Questions 
+  *For any questions, please contact me with the information below*
+   
+  Email:Leannaland2011@gmail.com
+
---
+++ HomeworkWeek17/server.js
@@ -0,0 +1,76 @@
+var express = require("express");
+var path = require("path")
+let db = require("./model");
+const mongoose = require("mongoose");
+
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// app.use(routes);
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.get("/exercise", function(req,res){
+    res.sendFile(path.join(__dirname,"./public/exercise.html"))
+})
+
+app.get("/stats", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+// GET route for getting all workouts for the dashboard page
+app.get("/api/workouts", (req, res) => {
+    db.find({})
+      .then(dbworkout => {
+        res.json(dbworkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+
+   // POST route for creating a brand new workout
+   app.post("/api/workouts", (req, res) => {
+    db.create({})
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+// PUT route for updating an existing workout with a newly created exercise 
+app.put("/api/workouts/:id", (req, res) => {
+    db.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body}}, {new:true})
+        .then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
